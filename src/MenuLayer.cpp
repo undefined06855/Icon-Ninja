fdc3c71787f32940ff69d31dd0746db22f97d34d
undefined06855 <currentcashew57@gmail.com>
2024-11-16 21:05:14 +0000

    remove silly geode logs

--- src/MenuLayer.cpp
+++ src/MenuLayer.cpp
@@ -2,9 +2,8 @@
 #include "MenuGameLayer.hpp"
 
 void HookedMenuLayer::keyDown(cocos2d::enumKeyCodes code) {
-    geode::log::info("keydown");
     if (static_cast<HookedMenuGameLayer*>(m_menuGameLayer)->m_fields->isInGameplay) {
-        geode::log::info("GET BLOCKED");
+        // block keybinds when in gameplay
         return;
     }
 
